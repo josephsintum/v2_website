422e844021f856e062f65dd5ed84e75d47c29e49
josephsintum <sjumbam15@gmail.com>
2019-11-02 04:01:30 -0700

    cleaning footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,38 +1,20 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 
 const FooterContainer = styled.footer``
 
-const Copyright = styled.p`
-	text-align: center;
-	font-size: 0.8rem;
-	margin: 2rem 0;
-`
+const Copyright = styled.p({
+	textAlign: "center",
+	fontSize: "0.8rem",
+	margin: "2rem 0",
+})
 
 const Footer = () => {
-	const data = useStaticQuery(graphql`
-		query SiteTitleQuery {
-			site {
-				siteMetadata {
-					title
-					siteUrl
-				}
-			}
-		}
-	`)
-
 	return (
 		<FooterContainer>
 			<Copyright>
-				<a href={data.site.siteMetadata.siteUrl + "/generate"}>
-					Use this Template
-				</a>{" "}
-				|{" "}
-				<a href={data.site.siteMetadata.siteUrl + "/issues"}>
-					Report an Issue
-				</a>{" "}
-				| <a href="https://www.gatsbyjs.org/docs/">Gatsby Docs</a>
+				© {new Date().getFullYear()} — Designed & developed by Joseph
+				Sintum
 			</Copyright>
 		</FooterContainer>
 	)
