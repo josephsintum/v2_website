e71fd2f57a0b41447c7715de7e9d3d7f7018bcd8
josephsintum <sjumbam15@gmail.com>
2019-11-09 15:05:43 -0800

    updating 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,30 +1,21 @@
 import React from "react"
 import { Link } from "../components/link"
-import styled from "styled-components"
-
 import Layout from "../components/layout"
+import { Container, Row, Col } from "../components/box"
+import Text, { Title } from "../components/text"
 import SEO from "../components/seo"
-
-const Container = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	min-height: 100%;
-	text-align: center;
-`
 
 const NotFoundPage = () => (
 	<Layout>
 		<SEO title="404: Not found" />
 
-		<Container>
-			<div>
-				<h1>404: Not found</h1>
-				<p>The page you were looking for could not be found.</p>
-				<p>
-					<Link to="/">Return Home</Link>
-				</p>
-			</div>
+		<Container my="auto">
+			<Title fontSize={[9]} as="h1">
+				404
+			</Title>
+			<Text>The page you were looking for could not be found.</Text>
+			<br/>
+			<Link to="/">Return Home</Link>
 		</Container>
 	</Layout>
 )
