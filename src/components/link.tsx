94bf035c496ed630091241cbda2944bc5735f428
josephsintum <sjumbam15@gmail.com>
2019-11-09 14:52:22 -0800

    updating link styles

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -5,32 +5,36 @@ import { TextProps, TextSystemStyle } from "./text"
 
 export interface LinkProps extends TextProps {}
 
-/** Link is a Gatsby Link wrapper with styling */
-export const Link = styled(GatsbyLink)<LinkProps>(
-	TextSystemStyle,
-	css({
-		color: "text",
-		p: 0,
-		textDecoration: "none",
+const linkStyle = css({
+	color: "text",
+	p: 0,
+	textDecoration: "none",
+	backgroundImage: `linear-gradient(
+        transparent 0%,
+        transparent calc(50% - 8px),
+        #dbe4ff calc(50% - 8px),
+        #dbe4ff 100%
+      )`,
+	backgroundSize: "100% 200%",
+	backgroundPosition: "0 0",
+	transition:
+		"background-position 120ms ease-in-out, padding 120ms ease-in-out",
+	"&:hover": {
+		py: "1",
 		backgroundImage: `linear-gradient(
-			transparent 0%,
-			transparent calc(50% - 8px),
-			#dbe4ff calc(50% - 8px),
-			#dbe4ff 100%
-		  )`,
-		backgroundSize: "100% 200%",
-		backgroundPosition: "0 0",
-		transition:
-			"background-position 120ms ease-in-out, padding 120ms ease-in-out",
-		"&:hover": {
-			py: "1",
-			backgroundImage: `linear-gradient(
-                transparent 0%,
-                transparent calc(50% - 8px),
-                #dbe4ff99 calc(50% - 8px),
-                #dbe4ff99 100%
-              )`,
-			backgroundPosition: "0 100%",
-		},
-	})
-)
+            transparent 0%,
+            transparent calc(50% - 8px),
+            #dbe4ff99 calc(50% - 8px),
+            #dbe4ff99 100%
+          )`,
+		backgroundPosition: "0 100%",
+	},
+})
+
+/** ALink is a styled <a href="..."> */
+export const ALink = styled("a")<LinkProps>(TextSystemStyle, linkStyle)
+
+/** Link is a styled Gatsby Link wrapper and it is intented only for internal links.\
+ * For links to pages on other domains or pages on the same domain not handled by the current Gatsby site, use the normal <ALink href="..."> element.
+ */
+export const Link = styled(GatsbyLink)<LinkProps>(TextSystemStyle, linkStyle)
