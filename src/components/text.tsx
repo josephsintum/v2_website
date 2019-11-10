a1e98486b4712256985e7df6702f63cbb5d5cbe7
josephsintum <sjumbam15@gmail.com>
2019-11-10 03:57:45 -0800

    clean upd

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,37 +1,15 @@
 import styled from "styled-components"
-import {
-	color,
-	ColorProps,
-	space,
-	SpaceProps,
-	border,
-	BorderProps,
-	typography,
-	TypographyProps,
-	compose,
-} from "styled-system"
+import { BoxStyleCompose, BoxProps } from "./box"
 import css from "@styled-system/css"
 
-export interface TextProps
-	extends SpaceProps,
-		ColorProps,
-		BorderProps,
-		TypographyProps {}
-
-/** TextSystemStyle is compose style fn from styled system */
-export const TextSystemStyle = compose(
-	space,
-	color,
-	border,
-	typography
-)
+export interface TextProps extends BoxProps {}
 
 /** Text */
-const Text = styled("span")<TextProps & { href?: string }>(TextSystemStyle)
+const Text = styled("span")<TextProps>(BoxStyleCompose)
 
 /** Text */
 export const Title = styled(Text)<TextProps>(
-	TextSystemStyle,
+	BoxStyleCompose,
 	css({
 		fontFamily: "heading",
 	})
