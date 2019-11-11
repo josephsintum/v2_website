3d8415f2c8437d4bce30b398fda0bb886397a872
josephsintum <sjumbam15@gmail.com>
2019-11-11 02:32:15 -0800

    adding home content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
-import { Container, Row, Col } from "../components/box"
-import { Button } from "../components/button"
+import Box, { Container } from "../components/box"
 import Text, { Title } from "../components/text"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,46 +8,18 @@ const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" />
 		<Container>
-			<Row>
-				<Col p={4} mb={3} bg="highlight">
-					<Title as="h1" fontSize={[6, 7, 8, 9]}>
-						Something awesome is coming
-					</Title>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<Button variant="primary">Hello world</Button>
-				</Col>
-				<Col>
-					<Button variant="secondary" siz="small">
-						Hello world
-					</Button>
-				</Col>
-				<Col>
-					<Button variant="outline">Hello world</Button>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<Button variant="primary" siz="small">
-						Hello world
-					</Button>
-				</Col>
-				<Col>
-					<Button variant="secondary">Hello world</Button>
-				</Col>
-				<Col>
-					<Button variant="outline" siz="large">
-						Hello world
-					</Button>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<Text fontSize={7}>Hello world</Text>
-				</Col>
-			</Row>
+			<Box py={4} my={6}>
+				<Title as="h1" fontSize={[6]}>
+					Joseph Sintum
+					<br />
+					Superpower: Create Stuff
+				</Title>
+				<Text>
+					I’m a frontend Developer, specializing in building react.js
+					apps with typescript.
+					<br /> Currently learning Golang and love reading books.
+				</Text>
+			</Box>
 		</Container>
 	</Layout>
 )
