2d5dd50a8e58ef1ba0b7e5b10f735eceaf689bca
josephsintum <sjumbam15@gmail.com>
2019-11-20 02:37:56 -0800

    shadow updates

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -74,7 +74,8 @@ export default {
 		circle: 99999,
 	},
 	shadows: {
-		card: "0 0 4px rgba(0, 0, 0, .125)",
+		card:
+			"0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12)",
 	},
 	text: {
 		heading: {
