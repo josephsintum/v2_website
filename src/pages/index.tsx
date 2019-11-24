1eac91741d5f9cb243c07df7d2023ed666c4370f
josephsintum <sjumbam15@gmail.com>
2019-11-24 07:52:26 -0800

    home update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,36 @@
 import React from "react"
-import Box, { Container, Row, Col } from "../components/box"
+import Box, { Container} from "../components/box"
 import Text, { Title } from "../components/text"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link, ALink } from "../components/link"
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" />
 		<Container>
-			<Box py={[7]}>
-				<Title fontSize={["h4", "h3"]}>
-					I am Joseph Sintum
-					<br />I create beautiful things with code
+			<Box pt={7} pb={6}>
+				<Title fontSize="h3" fontWeight={900}>
+					Hello, I'm Joseph Sintum.
 				</Title>
+				<Text>
+					I am fullstack Javascript developer based in San Jose, CA
+				</Text>
+			</Box>
+			<Box lineHeight={2}>
+				<Link to="/about">About Me</Link>
+				<br />
+				<Link to="/experience">My Experience</Link>
+				<br />
+				<Link to="/portfolio">Portfolio</Link>
+				<br />
+				<Link to="https://github.com/josephsintum">Github</Link>
+				<br />
+				<ALink href="https://www.linkedin.com/in/josephsintum/">
+					LinkedIn
+				</ALink>
 			</Box>
 		</Container>
-		<Box bg="primarys.0">
-			<Container py={6}>
-				<Row>
-					<Col width={[null, 1 / 2]} pr={[null, 4]}>
-						<Title fontSize="h5">About me</Title>
-						<Text>
-							I am a self-taught Full Stack JavaScript Bay Area. I
-							have been programming for 3 years with lots of
-							experience in developing and maintaining Javascript,
-							Typescript and React apps.
-						</Text>
-						<Text>
-							I love minimal design and drawing.
-							<br />
-							I am currently learning Golang.
-							<br />I love reading books and drinking tea.
-						</Text>
-					</Col>
-
-					<Col width={[null, 1 / 2]}>
-						<Title fontSize="h5">Skills</Title>
-						<Text> Frontend Development</Text>
-						<Text color="primary">
-							Javascript, HTML, SCSS
-							<br /> React, Redux, Styled Components, GraphQL
-							<br /> Adobe XD, Figma, Design Systems
-						</Text>
-						<Text>Backend</Text>
-						<Text color="primary">
-							Express, Passport, Auth0
-							<br /> NodeJS, Prisma Graphql
-						</Text>
-					</Col>
-				</Row>
-			</Container>
-		</Box>
 	</Layout>
 )
 
