78dfb2f9662214914619847580955c385cbf86b2
josephsintum <sjumbam15@gmail.com>
2020-08-18 14:48:53 -0700

    fixing gatsby styletron server render bug

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,20 +1,35 @@
 import * as React from "react"
-import { Client as Styletron } from "styletron-engine-atomic"
 import { Provider as StyletronProvider } from "styletron-react"
+import { Client } from "styletron-engine-atomic"
 import { LightTheme, BaseProvider } from "baseui"
 import Header from "./header"
 import Footer from "./footer"
 
-const engine = new Styletron()
+const Layout: React.FC = ({ children }) => {
+	const [engine, setEngine] = React.useState<Client | null>(null)
 
-const Layout: React.FC = ({ children }) => (
-	<StyletronProvider value={engine}>
-		<BaseProvider theme={LightTheme}>
-			<Header />
-			{children}
-			<Footer />
-		</BaseProvider>
-	</StyletronProvider>
-)
+	React.useEffect(() => {
+		// Load the `styletron-engine-atomic` package dynamically.
+		// Reason: It requires use of `document`, which is not available
+		// outside the browser, so we need to wait until it successfully loads.
+		// Source: https://www.gatsbyjs.org/docs/debugging-html-builds/
+		import("styletron-engine-atomic").then((styletron) => {
+			const clientEngine = new styletron.Client()
+			setEngine(clientEngine)
+		})
+	}, [])
+
+	if (!engine) return null
+
+	return (
+		<StyletronProvider value={engine}>
+			<BaseProvider theme={LightTheme}>
+				<Header />
+				{children}
+				<Footer />
+			</BaseProvider>
+		</StyletronProvider>
+	)
+}
 
 export default Layout
