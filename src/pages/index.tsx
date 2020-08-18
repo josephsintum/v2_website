6ac69039c5100e9f2e9f3833c6fb57f498bb42b2
josephsintum <sjumbam15@gmail.com>
2020-08-18 04:45:18 -0700

    adding about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,10 @@ import SEO from "../components/seo"
 import Layout from "../components/layout"
 import { Block } from "baseui/block"
 import { H2, H3, Label2, Paragraph2 } from "baseui/typography"
-import { Button } from "baseui/button/index"
+import { Button } from "baseui/button"
+import { FlexGrid, FlexGridItem } from "baseui/flex-grid"
+import { Skeleton } from "baseui/skeleton"
+import { useStyletron } from "baseui"
 
 const IndexPage = () => (
 	<Layout>
@@ -30,7 +33,8 @@ const Hero = () => (
 		flexDirection="column"
 		alignItems="flex-start"
 		minHeight="100vh"
-		margin={0}
+		margin="0px auto"
+		maxWidth="1000px"
 	>
 		<Label2 as="h1">Hello, my name is</Label2>
 		<H2
@@ -65,27 +69,99 @@ const Hero = () => (
 	</Block>
 )
 
-const About = () => (
-	<Block
-		as="section"
-		display="flex"
-		justifyContent="center"
-		flexDirection="column"
-		alignItems="flex-start"
-		margin={0}
-	>
-		<H3>About Me</H3>
+const About = () => {
+	const [css, theme] = useStyletron()
 
-		<Paragraph2 $style={{ textAlign: "justify" }}>
-			Hi. I'm Joseph, a Javascript developer based in San Jose.I am a
-			self-taught. I have been programming for 3 years with lots of
-			experience in developing and maintaining Javascript, Typescript and
-			React apps. I am working at San Jose City College as Web Master. My
-			time is spent maintaining and updating the San Jose City College
-			School and Athletics websites, contributing to open source projects
-			and just learning new technologies. Out of work, I spend my time
-			reading reading books, drawing, drinking coffee and just
-			learning languages like Golang or French. C'est tres amusant.
-		</Paragraph2>
-	</Block>
-)
+	return (
+		<Block
+			as="section"
+			justifyContent="center"
+			flexDirection="column"
+			alignItems="flex-start"
+			margin="0px auto"
+			padding={["100px 0px", "150px 0px"]}
+			maxWidth="1000px"
+		>
+			<H3>About Me</H3>
+			<FlexGrid
+				flexGridColumnCount={[1, 1, 2]}
+				flexGridColumnGap="scale2400"
+				flexGridRowGap="scale800"
+				alignItems="flex-start"
+				justifyContent="space-between"
+			>
+				<FlexGridItem>
+					<Paragraph2
+						$style={{ textAlign: "justify" }}
+						marginTop="0px"
+					>
+						Hi. I'm Joseph, a Javascript developer based in San
+						Jose. I am a self-taught. I have been programming for 3
+						years with lots of experience in developing and
+						maintaining Javascript, Typescript and React apps.
+						<br />
+						<br /> I am working at San Jose City College as Web
+						Master. My time is spent maintaining and updating the
+						San Jose City College School and Athletics websites,
+						contributing to open source projects and just learning
+						new technologies.
+						<br />
+						<br />
+						Out of work, I spend my time reading reading books,
+						drawing, hiking and just learning languages like Golang
+						or French. C'est tres amusant.
+					</Paragraph2>
+					<ul
+						className={css({
+							textAlign: "justify",
+							display: "grid",
+							listStyle: "none",
+							gridTemplateColumns: "200px 200px",
+							padding: "0px",
+						})}
+					>
+						{[
+							"Javascript & TS",
+							"React",
+							"Node.js",
+							"Express",
+							"HTML, CSS (in JS)",
+							"Victory Charts",
+							"Mongoose (MongoDB)",
+							"Figma",
+						].map((item, index) => (
+							<li
+								key={index}
+								className={css({
+									listStylePosition: "outside",
+									position: "relative",
+									textAlign: "justify",
+									paddingLeft: "25px",
+									marginBottom: "10px",
+									fontFamily:
+										theme.typography.ParagraphMedium
+											.fontFamily,
+									"::before": {
+										content: '"👉"',
+										ariaLabel: "finger pointing right",
+										role: "img",
+										boxSizing: "border-box",
+										color: "#000",
+										position: "absolute",
+										lineHeight: "18px",
+										left: "0px",
+									},
+								})}
+							>
+								{item}
+							</li>
+						))}
+					</ul>
+				</FlexGridItem>
+				<FlexGridItem>
+					<Skeleton height="400px" animation />
+				</FlexGridItem>
+			</FlexGrid>
+		</Block>
+	)
+}
