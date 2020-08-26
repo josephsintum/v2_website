32e102910836bb38f17750a57068594f5ad2cf21
josephsintum <sjumbam15@gmail.com>
2020-08-26 02:00:53 -0700

    updating structure

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -5,40 +5,104 @@ import { H3, H2, Paragraph2 } from "baseui/typography"
 import { FlexGrid, FlexGridItem } from "baseui/flex-grid"
 import { styled } from "baseui"
 import { Tag } from "baseui/tag"
+import { StyledLink as Link } from "baseui/link"
+
+const data = [
+	{
+		name: "News Popular Word Visualization",
+		description:
+			"This is a graph visualization of the most popular words on news.\n" +
+			"It checks the most popular news articles every 15mins and makes a list of the most used words. This data is sent to the frontend and rendered into a graph showing the last 24 hours of statistics.\n",
+		tags: [
+			"React",
+			"Next.js",
+			"Victory Charts",
+			"Baseweb UI",
+			"Mongoose",
+			"TypeScript",
+		],
+		links: [
+			{
+				name: "Github",
+				url: "https://github.com/josephsintum/words-visualizations",
+			},
+			{
+				name: "Demo",
+				url: "https://words-stats.vercel.app/",
+			},
+		],
+		image:
+			"https://images.unsplash.com/photo-1597858715706-fb50f241946e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2851&q=80",
+	},
+	{
+		name: "josephsintum.now.sh",
+		description: "Personal Website \n" + "Open Sourced on github.",
+		tags: ["React", "Gatsby", "Baseweb UI", "TypeScript"],
+		links: [
+			{
+				name: "Github",
+				url: "https://github.com/josephsintum/my_website",
+			},
+			{
+				name: "Website",
+				url: "https://josephsintum.now.sh/",
+			},
+		],
+		image:
+			"https://images.unsplash.com/photo-1597858715706-fb50f241946e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2851&q=80",
+	},
+]
 
 export const Projects = () => {
 	return (
 		<Section padding={["100px 0px", "150px 0px"]}>
 			<H3>Some projects</H3>
 			<Block maxWidth="1000px">
-				<FlexGrid
-					flexGridColumnCount={[1, 1, 2]}
-					flexGridColumnGap="scale800"
-					flexGridRowGap="scale800"
-				>
-					<FlexGridItem>
-						<Image src="https://images.unsplash.com/photo-1597858715706-fb50f241946e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2851&q=80" />
-					</FlexGridItem>
-					<FlexGridItem>
-						<H2>Some awesome project</H2>
-						<Paragraph2>
-							News platform that aggregates daily articles from
-							500+ news publications and research institutes
-							around the United States and leverages NLP models in
-							conjunction with the Google Maps API to
-						</Paragraph2>
-						<Tag closeable={false}>Typescript</Tag>
-						<Tag closeable={false}>NodeJS</Tag>
-						<Tag closeable={false}>Mongoose</Tag>
-						<Tag closeable={false}>Victory</Tag>
-					</FlexGridItem>
-				</FlexGrid>
+				{data.map((project, pIndex) => (
+					<FlexGrid
+						flexGridColumnCount={[1, 1, 2]}
+						flexGridColumnGap="scale1000"
+						flexGridRowGap="scale1000"
+						marginBottom="scale1000"
+						key={`${pIndex}.Project`}
+					>
+						{/*<React.Fragment >*/}
+						<FlexGridItem display="flex">
+							<Image
+								style={{ margin: "auto" }}
+								src={project.image}
+							/>
+						</FlexGridItem>
+						<FlexGridItem>
+							<H2>{project.name}</H2>
+							<Paragraph2>{project.description}</Paragraph2>
+							<Paragraph2>
+								{project.tags.map((tag, tIndex) => (
+									<Tag
+										key={`${tIndex}${pIndex}tag`}
+										closeable={false}
+									>
+										{tag}
+									</Tag>
+								))}
+							</Paragraph2>
+							{project.links.map((link, lIndex) => (
+								<React.Fragment key={`${lIndex}${pIndex}link`}>
+									<Link href={link.url}>{link.name}</Link>
+									<br />
+								</React.Fragment>
+							))}
+						</FlexGridItem>
+						{/*</React.Fragment>*/}
+					</FlexGrid>
+				))}
 			</Block>
 		</Section>
 	)
 }
 
 export const Image = styled("img", {
-	width: "100%",
-	height: "auto",
-})
+			width: "100%",
+			height: "auto",
+			maxWidth: "500px",
+		})
