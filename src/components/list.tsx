c86392b8554ec2156e74dda45adfdcfde8d38eee
josephsintum <sjumbam15@gmail.com>
2020-09-01 11:43:50 -0700

    fix list

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,20 +1,20 @@
 import { styled } from "baseui"
 
 export const Li = styled("li", ({ $theme }) => ({
-	position: "relative",
-	textAlign: "justify",
-	paddingLeft: "25px",
-	marginBottom: "10px",
-	listStyle: "none",
-	fontFamily: $theme.typography.ParagraphMedium.fontFamily,
-	"::before": {
-		content: '"✱"',
-		ariaLabel: "asterisk",
-		role: "img",
-		boxSizing: "border-box",
-		color: "#000",
-		position: "absolute",
-		lineHeight: "18px",
-		left: "0px",
-	},
-}))
+			position: "relative",
+			textAlign: "justify",
+			paddingLeft: "25px",
+			marginBottom: "10px",
+			listStyle: "none",
+			fontFamily: $theme.typography.LabelSmall.fontFamily,
+			"::before": {
+				content: '"✱"',
+				ariaLabel: "asterisk",
+				role: "img",
+				boxSizing: "border-box",
+				color: "#000",
+				position: "absolute",
+				lineHeight: "18px",
+				left: "0px",
+			},
+		}))
