caa8034837f1f121d59b2676ff5a58fc1c3dc218
josephsintum <sjumbam15@gmail.com>
2020-08-31 21:34:20 -0700

    remove alignItems: flex-start

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -6,7 +6,6 @@ export const Section = styled(Block, {
 	display: "flex",
 	justifyContent: "center",
 	flexDirection: "column",
-	alignItems: "flex-start",
 
 	margin: "0px auto",
 	maxWidth: "1000px",
