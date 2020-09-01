be204d240e2c0ee8c5346d9343c8d3831a218b7c
josephsintum <sjumbam15@gmail.com>
2020-08-31 21:33:01 -0700

    update list Li

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,31 +1,20 @@
-import * as React from "react"
-import { useStyletron } from "baseui"
+import { styled } from "baseui"
 
-export const Li: React.FC = ({ children }) => {
-	const [css, theme] = useStyletron()
-
-	return (
-		<li
-			className={css({
-				position: "relative",
-				textAlign: "justify",
-				paddingLeft: "25px",
-				marginBottom: "10px",
-				listStyle: "none",
-				fontFamily: theme.typography.ParagraphMedium.fontFamily,
-				"::before": {
-					content: '"✱"',
-					ariaLabel: "finger pointing right",
-					role: "img",
-					boxSizing: "border-box",
-					color: "#000",
-					position: "absolute",
-					lineHeight: "18px",
-					left: "0px",
-				},
-			})}
-		>
-			{children}
-		</li>
-	)
-}
+export const Li = styled("li", ({ $theme }) => ({
+	position: "relative",
+	textAlign: "justify",
+	paddingLeft: "25px",
+	marginBottom: "10px",
+	listStyle: "none",
+	fontFamily: $theme.typography.ParagraphMedium.fontFamily,
+	"::before": {
+		content: '"✱"',
+		ariaLabel: "asterisk",
+		role: "img",
+		boxSizing: "border-box",
+		color: "#000",
+		position: "absolute",
+		lineHeight: "18px",
+		left: "0px",
+	},
+}))
