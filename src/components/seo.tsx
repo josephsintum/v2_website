dea945149eae9a2f924116c7df3cd32e947e3ac2
josephsintum <sjumbam15@gmail.com>
2020-10-28 14:35:05 -0700

    updating next code

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,79 +1,27 @@
-import React from "react"
-import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import Head from "next/head"
+import config from "../config"
 
-const SEO = ({ description, lang, meta, title }: SEOProps) => {
-	const { site } = useStaticQuery(
-		graphql`
-			query {
-				site {
-					siteMetadata {
-						title
-						description
-						author
-					}
-				}
-			}
-		`
-	)
-
-	const metaDescription = description || site.siteMetadata.description
+export default function SEO({
+	description,
+	title,
+}: {
+	description: string
+	title: string
+}) {
+	const siteTitle = config.title
 
 	return (
-		<Helmet
-			htmlAttributes={{
-				land: lang || "en",
-			}}
-			title={title}
-			titleTemplate={`%s | ${site.siteMetadata.title}`}
-			meta={[
-				{
-					name: `description`,
-					content: metaDescription,
-				},
-				{
-					property: `og:title`,
-					content: title,
-				},
-				{
-					property: `og:description`,
-					content: metaDescription,
-				},
-				{
-					property: `og:type`,
-					content: `website`,
-				},
-				{
-					name: `twitter:card`,
-					content: `summary`,
-				},
-				{
-					name: `twitter:creator`,
-					content: site.siteMetadata.author,
-				},
-				{
-					name: `twitter:title`,
-					content: title,
-				},
-				{
-					name: `twitter:description`,
-					content: metaDescription,
-				},
-			].concat(meta || [])}
-		/>
+		<Head>
+			<title>{`${title} | ${siteTitle}`}</title>
+			<meta name="description" content={description} />
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content={title} />
+			<meta property="og:description" content={description} />
+			<meta property="og:site_name" content={siteTitle} />
+			<meta property="twitter:card" content="summary" />
+			<meta property="twitter:creator" content={config.social.twitter} />
+			<meta property="twitter:title" content={title} />
+			<meta property="twitter:description" content={description} />
+		</Head>
 	)
 }
-
-interface SEOProps {
-	description?: string
-	lang?: string
-	meta?: [
-		{
-			name: string
-			content: string
-		}
-	]
-	title: string
-}
-
-export default SEO
